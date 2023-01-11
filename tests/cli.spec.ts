972710a3b417f46c68472d4f285495661410c97e
henryruhs <info@henryruhs.com>
2023-01-11 15:15:28 +0100

    Testing for main command

--- tests/cli.spec.ts
+++ tests/cli.spec.ts
@@ -1,8 +1,38 @@
+import fs from 'fs';
 import { expect } from 'chai';
 import { exec } from 'child_process';
 
 describe('cli', () =>
 {
+	describe('run main command', () =>
+	{
+		afterEach(() => fs.rmSync('tests/provider/08/node_modules', { recursive: true, force: true }));
+
+		it('picking default', done =>
+		{
+			exec('node --loader ts-node/esm src/cli.ts --path=tests/provider/08', error =>
+			{
+				expect(error).to.be.null;
+				expect(fs.readdirSync('tests/provider/08/node_modules')).to.include.any.members([ 'commander', 'mocha' ]);
+				expect(fs.readdirSync('tests/provider/08/node_modules')).to.not.include.any.members([ 'eslint' ]);
+				done();
+			});
+		})
+		.timeout(20000);
+
+		it('picking target', done =>
+		{
+			exec('node --loader ts-node/esm src/cli.ts --path=tests/provider/08 --target=lintDependencies', error =>
+			{
+				expect(error).to.be.null;
+				expect(fs.readdirSync('tests/provider/08/node_modules')).to.not.include.any.members([ 'commander', 'mocha' ]);
+				expect(fs.readdirSync('tests/provider/08/node_modules')).to.include.any.members([ 'eslint' ]);
+				done();
+			});
+		})
+		.timeout(20000);
+	});
+
 	it('run help command', done =>
 	{
 		exec('node --loader ts-node/esm src/cli.ts --help', error =>
